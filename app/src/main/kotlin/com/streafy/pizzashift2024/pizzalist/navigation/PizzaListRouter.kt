244370b3a4d5536306b2e5e8479b9fbb765d796c
streafy <streafyhs@gmail.com>
2024-07-10 19:00:59 +0700

    add navigation to pizza card screen

--- app/src/main/kotlin/com/streafy/pizzashift2024/pizzalist/navigation/PizzaListRouter.kt
+++ app/src/main/kotlin/com/streafy/pizzashift2024/pizzalist/navigation/PizzaListRouter.kt
@@ -1,6 +1,8 @@
 package com.streafy.pizzashift2024.pizzalist.navigation
 
+import com.streafy.pizzashift2024.shared.pizza.domain.Pizza
+
 interface PizzaListRouter {
 
-    fun openDetails()
+    fun openPizzaCard(pizza: Pizza)
 }
